b80b7dc0bd6f9defbc33f8ab2d930f402753d73a
Edff <esoffia@unimagdalena.edu.co>
2026-05-20 16:31:14 -0500

    feat(entidades/solicitudes): modificar el metodo de muestreo de la solicitud

---
+++ Entidades/Solicitudes.py
@@ -0,0 +1,13 @@
+class Solicitud:
+    def __init__(self, id_solicitud, origen, destino, tipo_servicio):
+        self.id_solicitud=id_solicitud
+        self.origen=origen
+        self.destino=destino
+        self.tipo_servicio=tipo_servicio
+        self.estado="En espera"  #Estados: "Pendiente", "En Proceso", "Completada"
+        self.tarifa=0
+        self.conductor_asignado=None
+
+    def __str__(self):
+        conductor = self.conductor_asignado.nombre if self.conductor_asignado else "Ninguno"
+        return f"[Solicitud: {self.id_solicitud} - {self.origen} a {self.destino} ({self.tipo_servicio}) - Estado: {self.estado}]"
