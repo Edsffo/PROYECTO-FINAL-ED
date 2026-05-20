e38a4db895a2d5d46b1276fd642cbbb8efcfbec3
Edff <esoffia@unimagdalena.edu.co>
2026-05-20 15:51:57 -0500

    feat(cooperativa_taxis): Agregar una nueva variable llamada placa

--- cooperativa_taxis.py
+++ cooperativa_taxis.py
@@ -1,14 +1,14 @@
-
 class Conductor:
-    def __init__(self, cedula, nombre, tipo_servicio, zona_actual):
+    def __init__(self, cedula, nombre, tipo_servicio, zona_actual, placa):
         self.cedula=cedula
         self.nombre=nombre
         self.tipo_servicio=tipo_servicio  # Tipos:"Estándar", "Mascotas", "Equipaje"
         self.zona_actual=zona_actual
+        self.placa=placa
         self.disponible=True
 
     def __str__(self):
-        return f"[{self.cedula}] {self.nombre} ({self.tipo_servicio}) - Estado: {'Libre' if self.disponible else 'Ocupado'}"
+        return f"[Conductor: [{self.placa}][{self.cedula}] {self.nombre} ({self.tipo_servicio}) - Estado: {'Libre' if self.disponible else 'Ocupado'}"
 
 
 
