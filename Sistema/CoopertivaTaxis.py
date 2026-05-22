c7459b6042aa08671ff31ad02b12d34f5f0ae488
Jhoan <jawkzie@gmail.com>
2026-05-22 00:03:15 -0500

    fix: corregir los imports y la estructura de los paquetes para  utilizar el debugger de vscode

--- Sistema/CoopertivaTaxis.py
+++ Sistema/CoopertivaTaxis.py
@@ -1,3 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# ----------------------------------------------------- #
+
 from Estructuras.ColaCircular import Cola_circular
 from Estructuras.Pila import Pila
 from Estructuras.ListaSimple import ListaSimple
@@ -7,3 +13,8 @@ from Entidades.Conductor import Conductor
 from Entidades.Operador import Operador
 from Entidades.TipoTaxi import TipoServicio  
 
+# ----------------------------------------------------- #
+
+if __name__ == "__main__":
+
+    pass
