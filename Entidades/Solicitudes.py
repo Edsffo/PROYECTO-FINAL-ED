527a4769d56b2ff1993cc3edb9150dcfe4629afe
Jhoan <jawkzie@gmail.com>
2026-05-22 02:18:44 -0500

    fix+add: cambiando constructor solicitudes y agregando getters

--- Entidades/Solicitudes.py
+++ Entidades/Solicitudes.py
@@ -1,15 +1,29 @@
 class Solicitud:
-    def __init__(self, id_solicitud, origen, destino,tipo_taxi):
-        self.id_solicitud=id_solicitud
-        self.origen=origen
-        self.destino=destino
-        self.tipo_requerido=tipo_taxi
-        self.estado="En espera"  #Estados: "Pendiente", "En Proceso", "Completada"
-        self.tarifa=0
-        self.tiempo_estimado=0
-        self.conductor_asignado=None
-
+    def __init__(self, id_solicitud, usuario, telefono, zona_origen, zona_destino, tipo_servicio):
+        self.id = id_solicitud
+        self.usuario = usuario
+        self.telefono = telefono
+        self.zona_origen = zona_origen
+        self.zona_destino = zona_destino
+        self.tipo_servicio = tipo_servicio
+        self.estado = "En espera" # "en atención, completado, cancelado"
+        self.conductor_asignado = None
+        self.tarifa = 0
+        self.tiempo_recogida = 0
+    
     def __str__(self):
-        conductor = self.conductor_asignado.nombre if self.conductor_asignado else "Ninguno"
-        servicio_pedido = self.tipo_requerido.tipo
-        return f"Solicitud: [{self.id_solicitud} - de:{self.origen} a {self.destino} con servicio: ({servicio_pedido})] - Tiempo estimado: [{self.tiempo_estimado}] min - Estado: {self.estado}]- Conductor: {conductor} - Tarifa: {self.tarifa}]"
+        return f"Solicitud #{self.id} - {self.usuario} - {self.estado}"
+    
+    def resumen(self):
+        return {
+            "id": self.id,
+            "usuario": self.usuario,
+            "telefono": self.telefono,
+            "origen": self.zona_origen,
+            "destino": self.zona_destino,
+            "tipo": self.tipo_servicio,
+            "estado": self.estado,
+            "conductor": self.conductor_asignado.nombre if self.conductor_asignado else "No hay conductor asignado",
+            "tarifa": self.tarifa,
+            "tiempo_recogida": self.tiempo_recogida
+        }
