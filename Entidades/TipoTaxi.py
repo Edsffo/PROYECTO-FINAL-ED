2665a806b514d98cbb1f4cb1f6f14e3344e3a29e
Jhoan <jawkzie@gmail.com>
2026-05-22 02:22:34 -0500

    fix: los pycache son para poder utilizar el debugger de vscode

--- Entidades/TipoTaxi.py
+++
@@ -1,6 +0,0 @@
-class TipoServicio:
-    def __init__(self, tipo):
-        self.tipo=tipo  # Tipos:"Estándar", "Mascotas", "Equipaje"
-    
-    def __str__(self):
-        return f"[TipoServicio: {self.tipo}]"
